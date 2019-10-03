14f1add05910850322d9f0bd5f037c238e3c4f8a
veronika-martinovich <veranika.mart@gmail.com>
2019-10-03 20:15:44 +0300

    feat: solve the task 'morse-decoder'

--- src/index.js
+++ src/index.js
@@ -38,8 +38,56 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+  let arr = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    let letter = expr.slice(i, (i + 10));
+    arr.push(letter);
+    } 
+  
+  let arrZeroOne = [];
+  for (let j = 0; j < arr.length; j++) {
+    for (let k = 0; k < arr[j].length; k++) {
+      if (arr[j][k] == 0) continue;
+      if ((arr[j][k] == 1) || (arr[j][k] === '*')) {
+      let letterZeroOne = arr[j].slice(k);
+      arrZeroOne.push(letterZeroOne);
+      break;
+      }
+    }
+  }
+
+  let arrMorse = [];
+  for (let n = 0; n < arrZeroOne.length; n++) {
+    let letterMorse = '';
+    for (let p = 0; p < arrZeroOne[n].length; p += 2) {
+      if (arrZeroOne[n][p] == 1 && ((arrZeroOne[n][p+1]) == 0)) {
+      letterMorse += '.';
+      }
+      if (arrZeroOne[n][p] == 1 && ((arrZeroOne[n][p+1]) == 1)) {
+      letterMorse += '-';
+      }
+      if (arrZeroOne[n][p] == '*' && ((arrZeroOne[n][p+1]) == '*')) {
+        letterMorse += ' ';
+        break;
+        }
+    }
+    arrMorse.push(letterMorse);
+  }
+  
+  let arrAlphabetic = [];
+  for (let m = 0; m < arrMorse.length; m++) {
+    if (arrMorse[m] != ' ') {
+    let letterAlphabetic = MORSE_TABLE[arrMorse[m]];
+    arrAlphabetic.push(letterAlphabetic);
+    }
+   else {
+    arrAlphabetic.push(' ')
+    }
+  }
+  let phrase = arrAlphabetic.join('');
+  return phrase;
+  }
 
 module.exports = {
     decode
